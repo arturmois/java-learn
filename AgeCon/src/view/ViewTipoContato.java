76a7bbc507bac0aab3608cc64d9f0676ac7d09fb
Artur Moises <arturmoiscontato@gmail.com>
2024-04-22 20:21:39 -0300

    feat(AgeCon): add forms

---
+++ AgeCon/src/view/ViewTipoContato.java
@@ -0,0 +1,17 @@
+package view;
+
+/**
+ *
+ * @author artur
+ */
+public class ViewTipoContato extends DefaultForm {
+    
+    public ViewTipoContato () {
+        setTitle("Cadastro de Tipo de Contato");
+    }
+
+    @Override
+    public void initializeComponents() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
